1a40d61260350afe5af7d862acc6054b890205f2
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-07-01 18:15:17 +0000

    created first entity, employee

---
+++ src/main/java/com/ratz/springboottesting/model/Employee.java
@@ -0,0 +1,26 @@
+package com.ratz.springboottesting.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "employees")
+public class Employee {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String firstName;
+  private String lastName;
+  private String email;
+}
