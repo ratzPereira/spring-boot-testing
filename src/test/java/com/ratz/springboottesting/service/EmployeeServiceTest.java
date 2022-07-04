432d301bda6c18ea0640e062c3d7f56579c0f416
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-07-04 11:40:49 +0000

    created unit test for saveEmployee method in the Service

---
+++ src/test/java/com/ratz/springboottesting/service/EmployeeServiceTest.java
@@ -0,0 +1,55 @@
+package com.ratz.springboottesting.service;
+
+import com.ratz.springboottesting.model.Employee;
+import com.ratz.springboottesting.repository.EmployeeRepository;
+import com.ratz.springboottesting.service.impl.EmployeeServiceImpl;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+public class EmployeeServiceTest {
+
+  private EmployeeRepository employeeRepository;
+  private EmployeeService employeeService;
+  private Employee employee;
+
+  @BeforeEach
+  public void setUp(){
+
+    employeeRepository = Mockito.mock(EmployeeRepository.class);
+    employeeService = new EmployeeServiceImpl(employeeRepository);
+
+    employee = Employee.builder()
+        .id(1L)
+        .firstName("Ratz")
+        .lastName("Pereira")
+        .email("some@email.com")
+        .build();
+  }
+
+  //JUnit test for saveEmployee method
+  @Test
+  @DisplayName("Test for saveEmployee with success!")
+  public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
+
+    //given - pre-condition or setup
+    BDDMockito.given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
+
+    BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+
+
+    //when - action or the behaviour that we are going test
+    Employee savedEmployee = employeeService.saveEmployee(employee);
+
+
+    //then - verify the output
+    assertThat(savedEmployee).isNotNull();
+    assertThat(savedEmployee.getId()).isEqualTo(employee.getId());
+
+  }
+}
